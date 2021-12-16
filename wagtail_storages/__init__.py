e76a30ed99ba939580ce942ef6a2afd2907e880d
Petr Dlouhý <petr.dlouhy@email.cz>
2021-12-16 13:10:20 +0100

    fix Django 4.1 warning: remove default_app_config

--- wagtail_storages/__init__.py
+++ wagtail_storages/__init__.py
@@ -1,3 +1,9 @@
 __version__ = "0.0.6"
 
-default_app_config = "wagtail_storages.apps.WagtailStoragesConfig"
+import django
+
+if django.VERSION >= (3, 2):
+    # The declaration is only needed for older Django versions
+    pass
+else:
+    default_app_config = "wagtail_storages.apps.WagtailStoragesConfig"
