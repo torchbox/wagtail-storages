1deffb3b79d98e6fbb0a31f7759929c95937afe8
Katherine Domingo <katherine.domingo@torchbox.com>
2024-03-07 16:42:06 +0800

    Get rid of conditional code for Wagtail < 5.2

--- wagtail_storages/tests/settings.py
+++ wagtail_storages/tests/settings.py
@@ -2,8 +2,6 @@ import os
 
 import django.utils.crypto
 
-from wagtail import VERSION as WAGTAIL_VERSION
-
 TESTS_PATH = os.path.dirname(os.path.abspath(__file__))
 
 SECRET_KEY = django.utils.crypto.get_random_string(50)
@@ -46,12 +44,11 @@ MEDIA_ROOT = os.path.join(TESTS_PATH, "media")
 
 STATIC_ROOT = os.path.join(TESTS_PATH, "static")
 
-if WAGTAIL_VERSION >= (5, 2):
-    # when running tests with Wagtail 5.2+, we need to set the STATIC_URL explicitly,
-    # otherwise the test server will not start.
-    # This happens when running tests against the main branch of Wagtail
-    # so I am assuming that an upcoming release will require this.
-    STATIC_URL = "/static/"
+# when running tests with Wagtail 5.2+, we need to set the STATIC_URL explicitly,
+# otherwise the test server will not start.
+# This happens when running tests against the main branch of Wagtail
+# so I am assuming that an upcoming release will require this.
+STATIC_URL = "/static/"
 
 ROOT_URLCONF = "wagtail_storages.tests.urls"
 
